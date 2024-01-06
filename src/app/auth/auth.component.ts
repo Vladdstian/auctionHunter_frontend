bbc1fade5d101e36753163ba9f877efdf382612b
Vlad <vlad.c.nichifor@gmail.com>
2024-01-06 15:51:24 +0200

    register test

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,71 +1,104 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
 
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [
-    MatCardModule, 
-    MatFormFieldModule, 
-    MatInputModule, 
-    FormsModule, 
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
     ReactiveFormsModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule
   ],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
+  providers: [AuthService]
 })
 export class AuthComponent {
-
   hide = true;
 
   email = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl('', [Validators.required, Validators.min(8)]);
 
   passwordMatch = (control: FormControl): { [s: string]: boolean } | null => {
-    return this.password.value === control.value ? null : { 'mismatch': true };
+    return this.password.value === control.value ? null : { mismatch: true };
   };
 
-  retype_password = new FormControl('', [Validators.required, Validators.min(8), this.passwordMatch]);
-  first_name = new FormControl('', [Validators.required, Validators.min(1)]);
+  retype_password = new FormControl('', [
+    Validators.required,
+    Validators.min(8),
+    this.passwordMatch,
+  ]);
+  firstName = new FormControl('', [Validators.required, Validators.min(1)]);
 
-  authType:string='login';
+  authType: string = 'login';
+
+  constructor(private authService: AuthService) {
+    
+  }
 
   getEmailErrorMessage() {
-    
     if (this.email.hasError('required')) {
       return 'You must enter a valid email';
-    } 
+    }
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
   getNameErrorMessage() {
-    return this.first_name.hasError('required') 
-    ? 'You must enter your first name' : '';
+    return this.firstName.hasError('required')
+      ? 'You must enter your first name'
+      : '';
   }
 
   getPasswordErrorMessage() {
-
     if (this.password.hasError('required')) {
       return 'You must enter a password';
-    } 
+    }
 
     return this.retype_password.hasError('mismatch')
-     ? 'Passwords don\'t match' : '';
+      ? "Passwords don't match"
+      : '';
   }
 
-  onChangeAuthType() : void {
-    this.authType=='register' ? this.authType='login' : this.authType='register';
+  onChangeAuthType(): void {
+    this.authType == 'register'
+      ? (this.authType = 'login')
+      : (this.authType = 'register');
     this.email.reset();
     this.password.reset();
     this.retype_password.reset();
-    this.first_name.reset();
+    this.firstName.reset();
+  }
+
+  onLogin() {
+    let email = this.email.getRawValue()!;
+    let password = this.password.getRawValue()!;
+    console.log(email, password)
+    this.authService.login(email, password);
+  }
+
+  onRegister() {
+    let firstName = this.firstName.getRawValue()!;
+    let email = this.email.getRawValue()!;
+    let password = this.password.getRawValue()!;
+  
+    this.authService.register(firstName, email, password);
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  public login(email: string, password: string) {
+    let body = {
+      email: email,
+      password: password,
+    };
+    return this.httpClient.post('http://localhost:8080/api/auth/login', body);
+  }
+
+  public register(
+    firstName: string,
+    email: string,
+    password: string,
+    // retypePassword: string
+  ) {
+    let body = {
+      firstName: firstName,
+      email: email,
+      password: password,
+      // retypePassword: retypePassword,
+    };
+    return this.httpClient.post('http://localhost:8080/api/auth/register', body);
+  }
 }
